7aa517ab1b292f730f142ed780f06c0b89b182af
huyduong155 <nhathuy.155@gmail.com>
2021-09-23 08:20:38 +0700

    updatebt

---
+++ bai_1_co_so_du_lieu_quan_he/bai_tap/xay_dung_co_so_du_lieu_quan_ly_sinh_vien.sql
@@ -0,0 +1,11 @@
+USE student_management;
+CREATE TABLE class(
+id INT,
+`name` VARCHAR(45)
+);
+CREATE TABLE teacher(
+id INT,
+`name` VARCHAR(45),
+age INT,
+country VARCHAR(45)
+)
---
+++ bai_1_co_so_du_lieu_quan_he/thuc_hanh/tao_bang_tren_mysql_workbrench.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA student_management;
+CREATE TABLE Student(
+id INT,
+`name` VARCHAR(45),
+age INT,
+country VARCHAR(45)
+ 
+);
