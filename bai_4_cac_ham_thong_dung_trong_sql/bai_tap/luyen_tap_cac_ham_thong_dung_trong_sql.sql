afafa7b3adeb672e876c6b9457ef95f3729148a7
huyduong155 <nhathuy.155@gmail.com>
2021-09-26 21:45:31 +0700

    update

--- bai_4_cac_ham_thong_dung_trong_sql/bai_tap/luyen_tap_cac_ham_thong_dung_trong_sql.sql
+++ bai_4_cac_ham_thong_dung_trong_sql/bai_tap/luyen_tap_cac_ham_thong_dung_trong_sql.sql
@@ -1,7 +1,12 @@
 USE quanlysinhvien;
 SELECT * FROM `subject` 
 WHERE Credit = (SELECT max(`subject`.Credit) FROM `subject`);
-SELECT * FROM`subject`
+SELECT `subject`.SubId, `subject`.SubName, `subject`.Credit, `subject`.`Status`, mark FROM `subject`
 LEFT JOIN mark on `subject`.SubId = mark.SubId
 GROUP BY SubName
 HAVING Mark = (SELECT max(Mark) FROM mark);
+SELECT student.StudentId, student.StudentName, student.Address, student.Phone, student.ClassId, AVG(Mark)
+FROM mark 
+JOIN student on student.StudentId = mark.StudentId
+GROUP BY student.StudentId 
+ORDER BY AVG(Mark);
