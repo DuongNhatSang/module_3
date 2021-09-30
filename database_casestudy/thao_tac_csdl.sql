6d8f968d43f350752a009be866d25080b4d68ebc
huyduong155 <nhathuy.155@gmail.com>
2021-09-30 09:50:21 +0700

    update

--- database_casestudy/thao_tac_csdl.sql
+++ database_casestudy/thao_tac_csdl.sql
@@ -167,16 +167,16 @@ WHERE nhanvien.id_nhan_vien NOT IN (SELECT hopdong.id_nhan_vien FROM hopdong WHE
 --          phòng với tổng Tiền thanh toán trong năm 2019 là lớn hơn 10.000.000 VNĐ.
 
 UPDATE khachhang
-JOIN hopdong on khachhang.id_khach_hang = hopdong.id_khach_hang
 SET khachhang.id_loai_khach = 1
-WHERE khachhang.id_loai_khach = 2 AND
- (SELECT sum(dichvu.chi_phi_thue + (hopdongchitiet.so_luong*dichvudikem.gia)) AS tong_tien
- FROM khachhang
- JOIN hopdong on khachhang.id_khach_hang = hopdong.id_khach_hang
- JOIN dichvu on dichvu.id_dich_vu = hopdong.id_dich_vu
- JOIN hopdongchitiet on hopdongchitiet.id_hop_dong = hopdong.id_hop_dong
- JOIN dichvudikem on dichvudikem.id_dich_vu_di_kem = hopdongchitiet.id_dich_vu_di_kem
- GROUP BY
+WHERE khachhang.id_loai_khach = 2 AND khachhang.id_khach_hang IN 
+ (SELECT hopdong.id_khach_hang
+ FROM hopdong
+ LEFT JOIN dichvu on dichvu.id_dich_vu = hopdong.id_dich_vu
+ LEFT JOIN hopdongchitiet on hopdongchitiet.id_hop_dong = hopdong.id_hop_dong
+ LEFT JOIN dichvudikem on dichvudikem.id_dich_vu_di_kem = hopdongchitiet.id_dich_vu_di_kem
+ WHERE year(hopdong.ngay_lam_hop_dong) = 2019
+ GROUP BY hopdong.id_khach_hang
+ HAVING sum((dichvu.chi_phi_thue + (hopdongchitiet.so_luong*dichvudikem.gia)))  >= 10000000
  );
  
  -- task 18: Xóa những khách hàng có hợp đồng trước năm 2016 (chú ý ràng buộc giữa các bảng).
@@ -186,6 +186,156 @@ WHERE khachhang.id_loai_khach = 2 AND
  
  -- task 19: Cập nhật giá cho các Dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2019 lên gấp đôi.
  
+ UPDATE dichvudikem
+ SET dichvudikem.gia = dichvudikem.gia*10
+ WHERE dichvudikem.id_dich_vu_di_kem IN 
+ (SELECT dichvudikem.id_dich_vu_di_kem 
+   FROM (SELECT dichvudikem.id_dich_vu_di_kem 
+		 FROM dichvudikem
+		 JOIN hopdongchitiet ON hopdongchitiet.id_dich_vu_di_kem = dichvudikem.id_dich_vu_di_kem
+		 GROUP BY  dichvudikem.id_dich_vu_di_kem
+		 HAVING SUM(hopdongchitiet.so_luong) >= 10) AS abc
+         );
+
+-- task 20: Hiển thị thông tin của tất cả các Nhân viên và Khách hàng có trong hệ thống, thông tin hiển thị bao gồm ID (IDNhanVien, IDKhachHang),
+--          HoTen, Email, SoDienThoai, NgaySinh, DiaChi.
+
+ SELECT (nhanvien.id_nhan_vien) AS id, concat(nhanvien.ho," ",nhanvien.ten) AS ho_ten, nhanvien.email, nhanvien.sdt, 
+ nhanvien.ngay_sinh, nhanvien.dia_chi
+ FROM nhanvien
+ UNION ALL
+ SELECT (khachhang.id_khach_hang) AS id, concat(khachhang.ho," ",khachhang.ten) AS ho_ten, khachhang.email, khachhang.sdt, 
+ khachhang.ngay_sinh, khachhang.dia_chi
+ FROM khachhang;
  
-		
+ -- task 21: Tạo khung nhìn có tên là V_NHANVIEN để lấy được thông tin của tất cả các nhân viên có địa chỉ là “Hải Châu” và đã từng 
+ --          lập hợp đồng cho 1 hoặc nhiều Khách hàng bất kỳ với ngày lập hợp đồng là “12/12/2019”
  
+ CREATE OR REPLACE VIEW v_nhanvien AS
+ SELECT nhanvien.id_nhan_vien, nhanvien.ho, nhanvien.ten, nhanvien.id_vi_tri,nhanvien.id_bo_phan,nhanvien.id_trinh_do,nhanvien.ngay_sinh,
+ nhanvien.luong,nhanvien.sdt,nhanvien.so_cmnd,nhanvien.dia_chi
+ FROM nhanvien
+ JOIN hopdong on hopdong.id_nhan_vien = nhanvien.id_nhan_vien
+ WHERE nhanvien.dia_chi = "Hải Châu" AND hopdong.ngay_lam_hop_dong = "2019/12/12";
+
+ SELECT * FROM v_nhanvien;
+ 
+ -- task 22: Thông qua khung nhìn V_NHANVIEN thực hiện cập nhật địa chỉ thành “Liên Chiểu” đối với tất cả các Nhân viên được nhìn thấy bởi 
+ --          khung nhìn này.
+ 
+ UPDATE v_nhanvien
+ SET v_nhanvien.dia_chi = "Liên Chiểu";
+ 
+ -- task 23: Tạo Store procedure Sp_1 Dùng để xóa thông tin của một Khách hàng nào đó với Id Khách hàng được truyền vào như là 1 tham số của SD
+ 
+DELIMITER $$
+CREATE PROCEDURE sp_1(id_khach_hang INT)
+BEGIN
+	DELETE FROM khachhang
+    WHERE khachhang.id_khach_hang = id_khach_hang;
+END;$$
+DELIMITER ;
+CALL sp_1(7);
+
+-- task 24: Tạo Store procedure Sp_2 Dùng để thêm mới vào bảng HopDong với yêu cầu Sp_2 phải thực hiện kiểm tra tính hợp lệ của dữ liệu bổ sung, với 
+--          nguyên tắc không được trùng khóa chính và đảm bảo toàn vẹn tham chiếu đến các bảng liên quan.
+ 
+ DELIMITER $$
+CREATE PROCEDURE sp_2(id_nhan_vien INT, id_khach_hang INT, id_dich_vu INT, ngay_lam_hop_dong DATE, ngay_ket_thuc DATE, tien_dat_coc DOUBLE)
+BEGIN
+	INSERT INTO hopdong(id_nhan_vien, id_khach_hang, id_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc)
+    VALUES (id_nhan_vien, id_khach_hang, id_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc);
+END;$$
+DELIMITER ;
+CALL sp_2(2,3,4,"2019/12/09","2019/12/10",5000000);
+
+-- task 25: Tạo triggers có tên Tr_1 Xóa bản ghi trong bảng HopDong thì hiển thị tổng số lượng bản ghi còn lại có trong bảng HopDong ra giao diện
+--          console của database
+
+DELIMITER $$
+CREATE TRIGGER tr_1 AFTER DELETE ON hopdong FOR EACH ROW
+BEGIN
+	SET @tong_so_ban_ghi_con_lai = (SELECT COUNT(hopdong.id_hop_dong) FROM hopdong);
+    
+END;$$
+DELIMITER ;
+DELETE FROM hopdong
+WHERE hopdong.id_hop_dong = 1;
+SELECT @tong_so_ban_ghi_con_lai;
+
+-- task 26: Tạo triggers có tên Tr_2 Khi cập nhật Ngày kết thúc hợp đồng, cần kiểm tra xem thời gian cập nhật có phù hợp hay không,
+--          với quy tắc sau: Ngày kết thúc hợp đồng phải lớn hơn ngày làm hợp đồng ít nhất là 2 ngày. Nếu dữ liệu hợp lệ thì cho phép cập nhật, 
+ --         nếu dữ liệu không hợp lệ thì in ra thông báo “Ngày kết thúc hợp đồng phải lớn hơn ngày làm hợp đồng ít nhất là 2 ngày” trên console của database
+
+DELIMITER $$
+CREATE TRIGGER tr_2 BEFORE UPDATE ON hopdong FOR EACH ROW
+BEGIN
+	SET @thoi_gian_kt = datediff(new.ngay_ket_thuc,old.ngay_lam_hop_dong);
+   
+    IF @thoi_gian_kt < 2 THEN 
+       SIGNAL SQLSTATE '45000'
+       SET MESSAGE_TEXT = "Ngày kết thúc hợp đồng phải lớn hơn ngày làm hợp đồng ít nhất là 2 ngày";
+    END IF;
+END;$$
+DELIMITER ;
+
+UPDATE hopdong
+SET ngay_ket_thuc = "2019/12/15"
+WHERE id_hop_dong = 3;
+
+-- task 27: Tạo user function thực hiện yêu cầu sau:
+--          a. Tạo user function func_1: Đếm các dịch vụ đã được sử dụng với Tổng tiền là > 2.000.000 VNĐ.
+--          b. Tạo user function Func_2: Tính khoảng thời gian dài nhất tính từ lúc bắt đầu làm hợp đồng đến lúc kết thúc hợp đồng mà Khách hàng đã
+--             thực hiện thuê dịch vụ (lưu ý chỉ xét các khoảng thời gian dựa vào từng lần làm hợp đồng thuê dịch vụ, không xét trên toàn bộ các lần
+--             làm hợp đồng). Mã của Khách hàng được truyền vào như là 1 tham số của function này.
+
+-- câu a:
+DELIMITER $$
+CREATE FUNCTION func_1() 
+RETURNS INT
+DETERMINISTIC
+BEGIN
+  DECLARE tong_cac_dich_vu INT DEFAULT 0;
+SELECT COUNT(id_dich_vu) INTO tong_cac_dich_vu
+  FROM(
+  SELECT hopdong.id_dich_vu,COUNT(hopdong.id_dich_vu) AS tong_dich_vu,dichvu.chi_phi_thue,dichvu.chi_phi_thue*COUNT(hopdong.id_dich_vu) AS tong_tien
+  FROM hopdong
+  JOIN dichvu ON dichvu.id_dich_vu = hopdong.id_dich_vu
+  GROUP BY hopdong.id_dich_vu
+  HAVING dichvu.chi_phi_thue*COUNT(hopdong.id_dich_vu) > 3000000
+  ) AS a ;
+  RETURN tong_cac_dich_vu;
+END; $$
+DELIMITER ;
+SELECT func_1();
+
+-- câu b:
+DELIMITER $$
+CREATE FUNCTION func_2(id_khach_hang INT) 
+RETURNS INT
+DETERMINISTIC
+BEGIN
+  DECLARE thoi_gian_max INT DEFAULT 0;
+  SELECT MAX(khoang_thoi_gian) INTO thoi_gian_max
+  FROM (
+		SELECT datediff(ngay_ket_thuc,ngay_lam_hop_dong) AS khoang_thoi_gian
+		FROM hopdong
+		WHERE hopdong.id_khach_hang = id_khach_hang
+		) AS a;
+  RETURN thoi_gian_max;
+END; $$
+DELIMITER ; 
+SELECT func_2(2);
+
+-- task 28: Tạo Store procedure Sp_3 để tìm các dịch vụ được thuê bởi khách hàng với loại dịch vụ là “Room” từ đầu năm 2015 đến hết năm 2019 để xóa
+--          thông tin của các dịch vụ đó (tức là xóa các bảng ghi trong bảng DichVu) và xóa những HopDong sử dụng dịch vụ liên quan (tức là phải xóa
+--          những bản gi trong bảng HopDong) và những bản liên quan khá
+
+ DELIMITER $$
+CREATE PROCEDURE sp_3()
+BEGIN
+  DELETE FROM dichvu
+  WHERE dichvu.id_loai_dich_vu = 3 AND dichvu.id_dich_vu IN (SELECT hopdong.id_dich_vu FROM hopdong WHERE year(ngay_lam_hop_dong) IN (2015,2016,2017,2018,2019));
+END; $$
+DELIMITER ;  
+CALL sp_3();
